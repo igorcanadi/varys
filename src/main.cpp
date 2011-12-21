225be84d09a2bc40030cd6011793bd36e2a04cbb
Igor Canadi <canadi@cs.wisc.edu>
2011-12-20 18:59:35 -0600

    Add main loop.

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,43 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/info_parser.hpp>
 #include <glog/logging.h>
+#include <signal.h>
 #include "tests.h"
 #include "sensor_manager.h"
 
+int exit_initiated = 0;
+
+void sighandler(int signum) {
+    if (signum == SIGTERM) {
+        exit_initiated = 1;
+    }
+}
+
+void main_loop(boost::property_tree::ptree &config) {
+    // this is the real action
+    boost::shared_ptr<Queue<ptrRecord> > outputBuffer(new Queue<ptrRecord>);
+    boost::shared_ptr<Output> output =
+        boost::shared_ptr<Output> (new MySQLOutput(config));
+    OutputManager *om = new OutputManager(config, output, outputBuffer);
+    SensorManager *sm = new SensorManager(config, outputBuffer);
+
+    signal(SIGTERM, sighandler);
+
+    sm->run();
+    om->run();
+
+    do {
+        if (exit_initiated) {
+            break;
+        }
+        sleep(1);
+    } while (true);
+
+    delete om;
+    delete sm;
+
+    LOG(INFO) << "Exiting Varys";
+}
 
 int main(int argc, char *argv[]) {
     google::InitGoogleLogging(argv[0]);
@@ -27,11 +61,13 @@ int main(int argc, char *argv[]) {
     }
 
     //test_sensor_manager(config);
-    test_snmp_sensor(config);
+    //test_snmp_sensor(config);
     //test_mysql(config);
     //test_output_manager(config);
     //test_queue();
     //test_all_together(config);
+
+    main_loop(config);
 
     return 0;
 }
